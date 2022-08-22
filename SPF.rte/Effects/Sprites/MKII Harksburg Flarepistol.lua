7af85e855be3b68bd252561f008b3007e96eee48
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2022-08-22 23:08:19 +0300

    final touches hopefully

---
+++ SPF.rte/Effects/Sprites/MKII Harksburg Flarepistol.lua
@@ -0,0 +1,23 @@
+function MKIIHFA(actor)
+	local gun = ToAHuman(actor).EquippedItem;
+	if gun ~= nil then
+		local gun = ToHDFirearm(gun);
+		local magSwitchName = "MKII Harksburg Flarepistol Explosive Flare Magazine";
+		if gun.Magazine == nil or (gun.Magazine ~= nil and gun.Magazine.PresetName ~= magSwitchName) then
+			gun:SetNextMagazineName(magSwitchName);
+			gun:Reload();
+		end
+	end
+end
+
+function MKIIHFB(actor)
+	local gun = ToAHuman(actor).EquippedItem;
+	if gun ~= nil then
+		local gun = ToHDFirearm(gun);
+		local magSwitchName = "MKII Harksburg Flarepistol Fragmentation Flare Magazine";
+		if gun.Magazine == nil or (gun.Magazine ~= nil and gun.Magazine.PresetName ~= magSwitchName) then
+			gun:SetNextMagazineName(magSwitchName);
+			gun:Reload();
+		end
+	end
+end
